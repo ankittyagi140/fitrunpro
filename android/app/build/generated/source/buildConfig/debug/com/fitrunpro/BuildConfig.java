e5e542ea1de698f86fea14ac926c84a99f04a92a
Ankit Tyagi <ankit.tyagi555@gmail.com>
2025-01-07 16:04:07 +0530

    fix:build issue

---
+++ android/app/build/generated/source/buildConfig/debug/com/fitrunpro/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fitrunpro;
+
+public final class BuildConfig {
+  // Field from build type: debug
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fitrunpro";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
